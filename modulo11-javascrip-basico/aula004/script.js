e88fccbdd28ca2aedfa2b9a38b7223c6f693fc01
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-09-28 15:57:53 -0300

    Atribuição

---
+++ modulo11-javascrip-basico/aula004/script.js
@@ -0,0 +1,37 @@
+/*Tipos de Atribuição
+-- = => x = y;
+-- += => x += y => x = x + y;
+-- -= => x -= y => x = x - y;
+-- *= => x *= y => x = x * y;
+-- /= => x /= y => x = x / y;
+-- %= => x %= y => x = x % y;
+*/
+
+//Atribuição simples
+var a = 10; 
+console.log(a);
+
+//Atribuição com soma
+var b = 20;
+b += 20; //b = b + 20;
+console.log(b);
+
+//Atribuição com subtração
+var c = 10;
+c -= 7; //c = c - 7;
+console.log(c);
+
+//Atribuição com multiplicação
+var d = 4;
+d *= 8; //d = d * 8;
+console.log(d);
+
+//Atribuição com divisão
+var e = 20;
+e /= 4; // e = e / 4;
+console.log(e);
+
+//Atribuição com modulo
+var f = 50;
+f %= 30; // f = f % 30;
+console.log(f);
