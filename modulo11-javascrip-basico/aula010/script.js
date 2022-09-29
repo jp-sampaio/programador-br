0f81b74b519fe4d0292b1735d2d30cbc7ae1cd42
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-09-29 10:54:34 -0300

    Operador Ternário

---
+++ modulo11-javascrip-basico/aula010/script.js
@@ -0,0 +1,11 @@
+//Operador Ternário 
+//Armazena o resultado dentro de uma variavel e faz a condição.
+//? -- Funciona como o if e vai mostra algo se a condição for verdadeira.
+//: -- Funciona como o else e vai mostrar algo se a condição for falsa.
+
+var idade = 19;
+
+// var pode = idade >= 18 ? console.log('Pode ser preso') : console.log('Não pode ser preso');
+// var pode = idade >= 18 ? true : false;
+var pode = idade >= 18 ? 'pode' : 'não pode';
+console.log(pode);
