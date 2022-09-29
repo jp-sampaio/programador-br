22d341c62eb49520f5ad0a76b397ebf117f9f48f
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-09-29 15:50:48 -0300

    Laços de repetição

---
+++ modulo11-javascrip-basico/aula012/script.js
@@ -0,0 +1,35 @@
+//Laços de Repetição
+//For - precisa saber o inicio e o fim da repetição;
+//While - vai ter a repetição do loop enquanto for verdadeiro a condição, como não pode ser nenhuma vez;
+
+// var numero = 10;
+
+// for (var i = 0; i <= numero; i++) {
+//     console.log('Repetição', i);
+// } 
+
+//===============================================
+
+// for (var j = 100; j >= numero; j--){
+//     console.log(j);
+// }
+
+//===============================================
+
+// var num = Math.random() * 100;
+
+// while (num < 90){
+//     console.log(num);
+//     num = Math.random() * 100;
+// }
+
+// console.log(num);
+// console.log('Acabou!!!');
+
+//===============================================
+
+// while (numero <= 20){
+//     console.log('Digitando...', numero);
+//     numero++
+// }
+
