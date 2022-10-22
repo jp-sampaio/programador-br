dc3d2ac0d0b385f16234f81bb043c311b2c1e797
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-10-21 22:07:39 -0300

    exemplos de exercicios

---
+++ exercicio-if-else/praticando_conceitos/script.js
@@ -0,0 +1,72 @@
+// let idade = parseInt(prompt('Digite a sua idade para saber se você pode beber:'));
+
+// if(idade >= 18){
+//     maioridade = 'Pode beber';
+// }
+// else if(idade < 18){
+//     maioridade = 'Ainda não pode beber';
+// }
+// else{
+//     maioridade = 'Informação inválida';
+// }
+
+// // let maioridade = idade > 18 ? 'Pode beber' : 'Ainda é muito novo';
+// // console.log(maioridade);
+
+// switch(maioridade){
+//     case 'Pode beber':
+//         console.log('Qual é o seu pedido?');
+//         break;
+//     case 'Ainda não pode beber':
+//         console.log('Volte quando for de maior!');
+//         break;
+//     case 'Informação inválida':
+//         console.log('Tente novamente');
+//         break;
+//     default:
+//         console.log('Tu é burro?');
+//         break;
+// }
+
+// let vogal = prompt('Digite uma letra e descubra se ela é vogal:');
+
+// if(vogal === 'a' || vogal === 'A' || vogal === 'e' || vogal === 'E' || vogal === 'i' || vogal === 'I' || vogal === 'o' || vogal === 'O' || vogal === 'u' || vogal === 'U'){
+//     console.log('Essa letra é uma vogal');
+// }
+// else{
+//     console.log('Você não digitou uma vogal');
+// }
+
+
+// let multiplicador = parseInt(prompt('Você quer saber a Tabuada de qual número? '));
+
+// console.log('O resultado da tabuada do número que você escolheu:');
+// for(let i = 0; i < 11; i++){
+//     console.log(multiplicador + ' X ' + i + ' = ' + multiplicador * i);
+// }
+
+
+// let numero = 4;
+
+// while(numero <= 15){
+//     console.log(numero);
+//     numero++;
+// }
+
+// let alunos = ['joão', 1, true];
+
+// console.log(alunos[3]);
+
+// let estudantes = {
+//     nomes: ['João Paulo', 'Maria'],
+//     idades: [23, 19],
+//     media: function(){
+//         return (n1 + n2)/2;
+//     }
+// }
+
+// console.log(estudantes);
+
+// let data = new Date();
+
+// console.log(data);
