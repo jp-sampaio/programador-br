dc887feb061c822f14ac881ca946cd5bb83fed4f
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2023-01-04 21:17:07 -0300

    ainda em progresso

---
+++ projeto_jogo_da_memoria/assets/js/script.js
@@ -0,0 +1,64 @@
+const front = "card_front";
+const back = "card_back";
+
+let techs = [
+    "bootstrap",
+    "css",
+    "electron",
+    "firebase",
+    "html",
+    "javascript",
+    "jquery",
+    "mongo",
+    "node",
+    "react"
+];
+
+let cards = null;
+startGame();
+
+function startGame(){
+    cards = createCardsFromTech(techs);
+    shuffleCards(cards);
+    console.log(cards);
+}
+
+function shuffleCards(cards){
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]];
+    }
+}
+
+function createCardsFromTech(techs){
+    let cards = [];
+
+    for(let tech of techs){
+        cards.push(createPairFromTech(tech));
+    }
+
+    return cards.flatMap(pair => pair);
+}
+
+function createPairFromTech(tech){
+    return [
+    {
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }, 
+    {
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }];
+};
+
+function createIdWithTech(tech){
+    return tech + parseInt(Math.random() * 1000);
+}
