8d9b174ad48d74b404eb9c7152f8c502e7f59fb0
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2023-03-14 23:03:23 -0300

    estudo

--- modulo12-javascript-intermediario/aula08/script.js
+++ modulo12-javascript-intermediario/aula08/script.js
@@ -1,4 +1,6 @@
-// localStorage.setItem("nome", "João Paulo") // adiciona o joao paulo em nome
+//O localStorage funciona só para aquele dominio que ele está sendo executado, e ele armazena somente string e podemos contorna isso utilizando strings;
+
+// localStorage.setItem("nome", "João Paulo") // adiciona um dado na memoria do computador, ele recebe dois argumentos, uma chave e um valor;
 
 // let nome = localStorage.getItem("nome") // pega esse dado e mostra no console
 //console.log(nome)
@@ -8,33 +10,43 @@
 //========================================================================================
 
 onload = function(){
-    let nome = localStorage.getItem("nome")
+    let nome = localStorage.getItem("nome");
    
-    let h1 = document.getElementById("nome")
-    h1.innerHTML = nome
-
+    let h1 = document.getElementById("nome");
+    h1.innerHTML = nome;
 }
 
 
 function atualizar(elemento){
-    let valor = elemento.value
-    console.log(valor)
+    let valor = elemento.value;
+    console.log(valor);
 
-    let h1 = document.getElementById("nome")
-    h1.innerHTML = valor
+    let h1 = document.getElementById("nome");
+    h1.innerHTML = valor;
 
-    localStorage.setItem("nome", valor)
+    localStorage.setItem("nome", valor);
 }
 
 
 //====================================================================================================
+//Como o localStorage só recebe strings, então nesse caso nós transformamos um objeto em uma string pelo JSON e depois fazemos o inverso;
 
 let aluno = {
     nome: "Carlos",
     idade: 34
-}
+};
 
-localStorage.setItem("a", JSON.stringify(aluno))
+localStorage.setItem("aluno", JSON.stringify(aluno));
 
-let numero = localStorage.getItem("a")
-console.log(JSON.parse(numero))
+let numero = localStorage.getItem("aluno");
+console.log(JSON.parse(numero));
+
+//=============================================================================================
+//Mostra como podemos armazenar um número;
+
+localStorage.setItem("numero", 23);
+
+let number = localStorage.getItem("numero");
+
+console.log(Number(number));
+
